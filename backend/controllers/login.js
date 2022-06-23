b3488127a31c347431d8b1fbd4c8024b4431c30b
BryanCachimuel <bryanloyo56@gmail.com>
2022-06-23 11:24:18 -0500

    creando endpoint para iniciar sesión con correo y contraseña de usuarios

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -1,5 +1,29 @@
-const login = async (req, res) => {
+const bcrypt = require('bcrypt');
+const Usuario = require("../models/usuario")
 
-}
+const login = async (req, res) => {
+    const { correo, contrasenia } = req.body;
+
+    Usuario.findOne({correo}).then((usuario) => {
+        if(!usuario){
+            return res.json({mensaje: "Usuario no encontrado"})
+        }
+
+        // comparación de la contrasenia declara aquí con la contrasenia almacenada en la base de datos
+        bcrypt.compare(contrasenia, usuario.contrasenia).then((escorrecta) => {
+            if(escorrecta){
+                const { id, nombre } = usuario;
+                res.json({ mensaje: "Usuario logeado correctamente", 
+                usuario:{
+                    id,
+                    nombre,
+                },
+            });
+            }else{
+               return res.json({ mensaje: "Contraseña incorrecta" })
+            }
+        });
+    });
+};
 
 module.exports = login
