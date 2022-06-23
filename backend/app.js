5f4cf988722efba59b675e466afd6b280e16b306
BryanCachimuel <bryanloyo56@gmail.com>
2022-06-23 11:09:20 -0500

    agregando endponit para encontrar usuarios por usuarioId

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/usuario/:id', controllers.getUserById)
+app.get('/usuario/:userId', controllers.getUserById)
 app.post('/registro', controllers.registro)
 app.post('/login', controllers.login)
 
--- backend/controllers/getUserById.js
+++ backend/controllers/getUserById.js
@@ -1,5 +1,22 @@
-const getUserById = async (req, res) => {
+const Usuario = require("../models/usuario")
 
+/*
+    Si no se pone esto usuario._doc no se obtiene los datos como nombre y correo
+*/
+const getUserById = async (req, res) => {
+    const { userId } = req.params;
+    if(userId.length === 24){
+        Usuario.findById(userId).then((usuario) => {
+            if(!usuario){
+                return res.json({mensaje: "Usuario no encontrado"});
+            }else{
+                const {_id, contrasenia,__v,...resto} = usuario._doc
+                res.json(resto)
+            }
+        });
+    }else{
+        res.json({mensaje: "Estas enviando datos incorrectos"});
+    }
 }
 
 module.exports = getUserById
