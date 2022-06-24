8b6d2fde4c1e573a3279ceedc9d0f3f1390d0f7e
BryanCachimuel <bryanloyo56@gmail.com>
2022-06-23 22:15:44 -0500

    Creación del frontend con react y configuración del registro de usuarios

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register  from './components/Register';
+import Login  from './components/Login';
+import Welcome  from './components/Welcome';
+
+import styles from './App.module.scss'
+
+
+function App() {
+  return (
+   <BrowserRouter>
+      <div classname={styles.container}>
+        <Routes>
+          <Route path="/" element={<Register/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/bienvenido/:id" element={<Welcome/>}/>
+        </Routes>
+      </div>
+   </BrowserRouter>
+  );
+};
+
+export default App;
