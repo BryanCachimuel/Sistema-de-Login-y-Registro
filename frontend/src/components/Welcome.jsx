6b72c1b55799c5656f830934fe1d8ed6270e3e30
BryanCachimuel <bryanloyo56@gmail.com>
2022-06-23 22:58:57 -0500

    creando la vista de bienvenida para el usuario

--- frontend/src/components/Welcome.jsx
+++ frontend/src/components/Welcome.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import axios from 'axios';
+import React, {useState, useEffect} from 'react';
+import { useNavigate, useParams } from 'react-router-dom'
+
+import styles from './styles.module.scss';
 
 const Welcome = () => {
+
+  const [name, setName] = useState();
+
+  const navegar = useNavigate();
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    axios.get(`http://localhost:3005/usuario/${id}`)
+    .then(({data}) => setName(data.nombre))
+    .catch((error) => console.error(error))
+  },[id])
+
   return (
-    <div>
-      <h2>Bienvenido Usuario</h2>
+    <div className={styles.welcome}>
+      <h2>{name ? `Felicitaciones ${name}!` : 'Que estas haciendo'}</h2>
+      <h3>{name ? 'Iniciaste Sesión Correctamente' : 'Te estamos vigilando...'}</h3>
+      <div className={styles.buttons}>
+        <button onClick={() => navegar("/login")}>Inicia Sesión</button>
+        <button onClick={() => navegar("/")}>Registro</button>
+      </div>
     </div>
   )
 }
